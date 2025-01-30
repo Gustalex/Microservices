a6bcf50500bd718f9b2595e8b71d2f836f1e0aa8
Gustavo Santos <alexandregustavo00@gmail.com>
2025-01-29 23:55:32 -0300

    feat: inicia o microserviço de autenticação

---
+++ auth/auth_service/serializers/auth_serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from ..models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'username', 'first_name', 'last_name', 'password', 'is_superuser')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+    def validate(self, attrs):
+        user = authenticate(email=attrs['email'], password=attrs['password'])
+        if not user:
+            raise serializers.ValidationError("Credenciais inválidas.")
+        return {'user': user}
