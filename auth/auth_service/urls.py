4978a6cc76638694e83c7aceb08ccb0490d5cccc
Gustavo Santos <alexandregustavo00@gmail.com>
2025-02-02 11:51:16 -0300

    feat: implementa os endpoints de list e retrieve de users

--- auth/auth_service/urls.py
+++ auth/auth_service/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path  
-from .views import LoginView, RegisterView, LogoutView
+from .views import LoginView, RegisterView, LogoutView, GetUsersView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('users/', GetUsersView.as_view({'get': 'list'}), name='users'),
+    path('users/<int:pk>/', GetUsersView.as_view({'get': 'retrieve'}), name='user')
 ]
--- auth/auth_service/views/auth.py
+++ auth/auth_service/views/auth.py
@@ -1,10 +1,13 @@
 from rest_framework import generics
+from rest_framework.viewsets import ViewSet
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 from ..models import User
 from ..serializers import UserSerializer, LoginSerializer
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -35,3 +38,20 @@ class LogoutView(generics.GenericAPIView):
             return Response("Logout com sucesso.", status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"detail" : str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        
+
+class GetUsersView(ViewSet):
+    permission_classes = [IsAuthenticated]
+
+    @action(detail=False, methods=['get'])
+    def list(self, request):
+        queryset = User.objects.all()
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['get'])
+    def retrieve(self, request, pk=None):
+        queryset = User.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
